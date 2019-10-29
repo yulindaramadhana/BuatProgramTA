f5597b3399b291118031e556f1b8576435c34874
yulindaramadhana <yulinda.ramadhana98@gmail.com>
2019-10-29 13:56:10 +0700

    Update MainClass.java

--- src/Boundary/MainClass.java
+++ src/Boundary/MainClass.java
@@ -26,22 +26,24 @@ public class MainClass {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here\
 //        new MainFormTA().setVisible(true);
-        
+
         String pathsumber = "C:\\Users\\User\\Documents\\NetBeansProjects\\BuatProgramTA\\tes_doc\\text1.txt";
         String pathbanding = "C:\\Users\\User\\Documents\\NetBeansProjects\\BuatProgramTA\\tes_doc\\text2.txt";
+
         System.out.println(pathbanding);
-        
+
         TanpaSYR syr = new TanpaSYR();
         syr.parseFilesU(pathbanding);
         String[] stem = syr.getTermDocArray();
         String join1 = String.join(" ", syr.getTermDocArray());
         System.out.println("jon1=" + join1);
-        
+        List<String> join_stem1 = Arrays.asList(join1.split(" "));
+
         System.out.println("stem doc banding:");
         for (int i = 0; i < stem.length; i++) {
             System.out.println(stem[i]);
         }
-        
+
         System.out.println("\n");
         System.out.println(pathsumber);
         syr.parseFilesU(pathsumber);
@@ -49,57 +51,23 @@ public class MainClass {
         String join2 = String.join(" ", syr.getTermDocArray());
         System.out.println("jon2=" + join2);
         
-        System.out.println("stem doc sumber:");
-        for (int i = 0; i < stem2.length; i++) {
-            System.out.println(stem2[i]);
+        //store and print kamus sinonim
+        Kamus reads = new Kamus();
+        List<String> list_kamus = reads.SYR();
+              
+        for (int i = 0; i < list_kamus.size(); i++) {
+            System.out.println(list_kamus.get(i));
         }
         
-        
-        //cek same word
-        List<String> sameWord = new ArrayList<>();
-        List<String> word2 = Arrays.asList(join2.split(" "));
-        List<String> word1 = Arrays.asList(join1.split(" "));
-        for (String word : stem) {
-            if (word2.contains(word)) {
-                sameWord.add(word);
-            }
-        }
-        
-        System.out.println("\nkata sama= " + sameWord);
-    
-        
-        //cek sinonim same word
-        List<String> lststopword = new ArrayList<String>();
-        List<String> sinonim = new ArrayList<>();
-        Kamus stpwlist = new Kamus();
-        lststopword = stpwlist.SYR();
-
-//        for (int i = 0; i < lststopword.size(); i++) {
-//            if (lststopword.get(i).equals(sameWord)) {
-//                //...
-//            }
-//        }
-
-        System.out.println("\nkata sinonim= ");
-        for (String cekString : lststopword) {
-            if (sameWord.contains(cekString)) {
-                sinonim.add(cekString);
-                System.out.println(sinonim);
-            }
-        }
-        
-//        =======================================================
-
-        System.out.println("\ncek fungsi= ");
+        //cek sinonim
         DenganSYR dsyr = new DenganSYR();
-        List<String> cek_kata = dsyr.cek_sameWord(word2);
-        List<String> cek_sinonim = dsyr.cek_sameWord(cek_kata);
-        System.out.println(cek_kata);
-        System.out.println(cek_sinonim);
         
-        //replace sinonim
-        
-        
+        for (String word : join_stem1) {
+            if (list_kamus.contains(word)) {
+                System.out.println(word);
+            }
+        }
+
     }
-    
+
 }
